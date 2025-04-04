91698f5ba870759720a3b2c91c9d92691b57f216
thaiwireproducts <thaiwireproducts1@gmail.com>
2025-04-04 14:02:56 +0700

    update

--- src/app/components/AppLogo.tsx
+++ src/app/components/AppLogo.tsx
@@ -1,14 +1,27 @@
+
+
 export default function AppLogo() {
   const subTitle = "create by Codingthailand";
   const dateNow = new Date();
   const timeNow = <p>{dateNow.toLocaleTimeString()}</p>;
+  const isShow = true;
+  const isShowDate = true; // if isShowDate is true, show dateNow
 
   return (
     <>
       <p style={{ color: "red" }}>My Logo</p>
       <small style={{ color: "blue" }}>{subTitle}</small>{" "}
       <small style={{ color: "blue" }}>{dateNow.toLocaleDateString()}</small>{" "}
-      {timeNow}
+      
+
+      {
+        isShow && <div>เวลานี้ : {timeNow} </div> // if isShow is true, show timeNow
+      }
+
+      {
+        isShowDate ? <small style={{ color: "blue" }}>{dateNow.toLocaleDateString()}</small>
+        : <small style={{ color: "blue" }}>{dateNow.toLocaleTimeString()}</small>
+      }
     </>
   );
 }
