6227dd71052199af1518ecb470a14f5a1eae71f7
thaiwireproducts <thaiwireproducts1@gmail.com>
2025-04-04 13:49:45 +0700

    uodate

--- src/app/components/AppLogo.tsx
+++ src/app/components/AppLogo.tsx
@@ -1,7 +1,14 @@
 export default function AppLogo() {
+  const subTitle = "create by Codingthailand";
+  const dateNow = new Date();
+  const timeNow = <p>{dateNow.toLocaleTimeString()}</p>;
+
   return (
-    <>  
-      <p style={{color : "red"}}>My Logo</p>
+    <>
+      <p style={{ color: "red" }}>My Logo</p>
+      <small style={{ color: "blue" }}>{subTitle}</small>{" "}
+      <small style={{ color: "blue" }}>{dateNow.toLocaleDateString()}</small>{" "}
+      {timeNow}
     </>
   );
 }
